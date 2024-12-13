b5cc23bec279054ace63615d7f517d65c13112d5
Rostyslav Voitko <75126304+voytkojazz@users.noreply.github.com>
2024-12-13 11:21:10 +0100

    adding test and implementations

--- src/main/java/de/szut/mylists/MyArrayList.java
+++ src/main/java/de/szut/mylists/MyArrayList.java
@@ -1,5 +1,78 @@
 package de.szut.mylists;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
 public class MyArrayList {
+
+    private int[] values;
+
+    private int size;
+
+    static final int INIT_ARRAY_SIZE = 10;
+
+    public MyArrayList() {
+        this.values = new int[INIT_ARRAY_SIZE];
+        this.size = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(int value) {
+        if (size == values.length) {
+            copyAndExtendInternalArray();
+        }
+        this.values[size] = value;
+        this.size++;
+    }
+
+    public int get(int index) {
+        if (index > this.size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return this.values[index];
+    }
+
+    public void remove(int index) throws RuntimeException {
+        if (!indexExists(index)) {
+            throw new RuntimeException("Dieser Index existiert nicht!");
+        }
+        int[] firstSubSequence = Arrays.copyOfRange(values, 0, index);
+        int[] secondSubSequence = Arrays.copyOfRange(values, index + 1, values.length);
+        this.values = IntStream.concat(IntStream.of(firstSubSequence), IntStream.of(secondSubSequence)).toArray();
+        this.size -= 1;
+    }
+
+    public boolean contains(int value) {
+        for (int v : values) {
+            if (Objects.equals(value, v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Only visible for testing.
+     *
+     * @return the array that this class uses internally to store the values.
+     * The array is returned without empty cells on the end (only the array of a size if this list is returned).
+     *
+     */
+    int[] getInternalArrayForTest() {
+        return Arrays.copyOf(this.values, this.size);
+    }
+
+    private void copyAndExtendInternalArray() {
+        int newSize = size + 10;
+        this.values = Arrays.copyOf(values, newSize);
+    }
+
+    private boolean indexExists(int index) {
+        return index <= this.size - 1;
+    }
 
 }
--- src/test/java/de/szut/mylists/MyArrayListTest.java
+++ src/test/java/de/szut/mylists/MyArrayListTest.java
@@ -1,5 +1,244 @@
 package de.szut.mylists;
 
-public class MyArrayListTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MyArrayListTest {
+
+    private MyArrayList myArrayList;
+
+    static int[] testElements = new int[] {461, 674, 1234, 768, 123566, 3242, 1, 454, 412, 3490, 0, 12, 54, 0};
+
+    @BeforeEach
+    void setUp() {
+        myArrayList = new MyArrayList();
+    }
+
+    @Test
+    void test_add_whenAddingElementsOfInitialArraySize() {
+        // Arrange
+        int[] toAdd = new int[MyArrayList.INIT_ARRAY_SIZE];
+        for (int i = 0; i < MyArrayList.INIT_ARRAY_SIZE; i++) {
+            toAdd[i] = i;
+        }
+
+        // Act
+        for (int i : toAdd) {
+            myArrayList.add(i);
+        }
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(toAdd);
+    }
+
+    @Test
+    void test_add_whenAddingMoreElementsThenInitialArraySize() {
+        // Arrange
+        int amountToAdd = MyArrayList.INIT_ARRAY_SIZE + 5;
+        int[] toAdd = new int[amountToAdd];
+        for (int i = 0; i < amountToAdd; i++) {
+            toAdd[i] = i;
+        }
+
+        // Act
+        for (int i : toAdd) {
+            myArrayList.add(i);
+        }
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(toAdd);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1", "11", "21", "50", "101", "201"})
+    void test_add_givenN_elements_thenAllAddedElementsArePresent(int elementsToAdd) {
+        // Arrange
+        int[] toAdd = new int[elementsToAdd];
+        for (int i = 0; i < elementsToAdd; i++) {
+            toAdd[i] = i;
+        }
+
+        // Act
+        for (int i : toAdd) {
+            myArrayList.add(i);
+        }
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(toAdd);
+    }
+
+    @Test
+    void test_size_whenEmpty_thenReturn0() {
+        // Arrange
+
+        // Act
+        int result = myArrayList.size();
+        // Assert
+        assertThat(result).isZero();
+    }
+
+    @Test
+    void test_size_givenArrayToAdd_returnsSizeOfGivenArray() {
+        // Arrange
+        for (int testElement : testElements) {
+            myArrayList.add(testElement);
+        }
+
+        // Act
+        int resultSize = myArrayList.size();
+
+        // Assert
+        assertThat(resultSize).isEqualTo(testElements.length);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1", "5", "11", "21"})
+    void test_size_givenElementsToAdd_thenSizeEqualsGivenElements(int expectedSize) {
+        for (int i = 0; i < expectedSize; i++) {
+            myArrayList.add(i);
+        }
+        assertThat(myArrayList.size()).isEqualTo(expectedSize);
+    }
+
+    @Test
+    void test_remove_whenGivenIndexExists_thenElementIsDeletedAndElementsAreMoved() {
+        // Arrange
+        for (int testElement : new int[] {2, 453, 756, 1024}) {
+            myArrayList.add(testElement);
+        }
+        int[] expectedArray = new int[] {2, 453, 1024};
+
+        // Act
+        myArrayList.remove(2);
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(expectedArray);
+    }
+
+    @Test
+    void test_remove_whenRemovingFirstElement_FirstElementRemoved() {
+        // Arrange
+        for (int testElement : new int[] {2, 453, 756, 1024}) {
+            myArrayList.add(testElement);
+        }
+        int[] expectedArray = new int[] {453, 756, 1024};
+
+        // Act
+        myArrayList.remove(0);
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(expectedArray);
+    }
+
+    @Test
+    void test_remove_whenRemovingLastElement_LastElementRemoved() {
+        // Arrange
+        for (int testElement : new int[] {2, 453, 756, 1024}) {
+            myArrayList.add(testElement);
+        }
+        int[] expectedArray = new int[] {2, 453, 756};
+
+        // Act
+        myArrayList.remove(3);
+
+        // Assert
+        assertThat(myArrayList.getInternalArrayForTest()).isEqualTo(expectedArray);
+    }
+
+    @Test
+    void test_remove_whenGivenIndexIfGreaterThenArrayListSize_thenThrowsRuntimeException() {
+        // Arrange
+        for (int testElement : new int[] {2, 453, 756, 1024}) {
+            myArrayList.add(testElement);
+        }
+
+        // Act and Assert
+        Exception exceptionLeererParameter = assertThrows(RuntimeException.class,
+                () -> myArrayList.remove(4));
+        assertEquals("Dieser Index existiert nicht!", exceptionLeererParameter.getMessage());
+    }
+
+    @Test
+    void test_contains_whenGivenElementIsPresent_returnsTrue() {
+        // Arrange
+        myArrayList.add(2345);
+
+        // Act
+        boolean result = myArrayList.contains(2345);
+
+        // Assert
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void test_contains_whenGivenElementIsNotPresent_returnsFalse() {
+        // Arrange
+        myArrayList.add(2345);
+
+        // Act
+        boolean result = myArrayList.contains(12345);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void test_get_whenGetLastElement_returnsLastElement() {
+        // Arrange
+        for (int testElement : testElements) {
+            myArrayList.add(testElement);
+        }
+
+        // Act
+        int result = myArrayList.get(testElements.length - 1);
+
+        // Assert
+        assertThat(result).isEqualTo(testElements[testElements.length - 1]);
+    }
+
+    @Test
+    void test_get_whenGetFirstElement_returnsFirstElement() {
+        // Arrange
+        for (int testElement : testElements) {
+            myArrayList.add(testElement);
+        }
+
+        // Act
+        int result = myArrayList.get(0);
+
+        // Assert
+        assertThat(result).isEqualTo(testElements[0]);
+    }
+
+    @Test
+    void test_get_whenIndexIsPresent_returnsValuesOnThatIndex() {
+        // Arrange
+        for (int testElement : testElements) {
+            myArrayList.add(testElement);
+        }
+
+        // Act
+        int result = myArrayList.get(11);
+
+        // Assert
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    void test_get_whenIndexIsNotPresent_throwsIndexOutOfBoundException() {
+        // Arrange
+        for (int testElement : testElements) {
+            myArrayList.add(testElement);
+        }
+
+        // Act
+        assertThrows(IndexOutOfBoundsException.class, () -> myArrayList.get(testElements.length));
+    }
 
 }
